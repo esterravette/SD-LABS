0663e4aa1786139f9899982c0f83c209edec7ecc
esterravette <tayholandar@gmail.com>
2025-07-11 19:05:48 -0300

    laboratorio de ejb

---
+++ ejb/ejb-demo/src/main/java/com/ejb/HelloServlet.java
@@ -0,0 +1,23 @@
+package com.ejb;
+
+import java.io.IOException;
+
+import jakarta.ejb.EJB;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+    @EJB
+    HelloBean helloBean;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        resp.setContentType("text/plain");
+        resp.getWriter().write(helloBean.sayHello("usuário"));
+    }
+}
